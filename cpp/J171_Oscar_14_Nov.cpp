e52ffc1ac594c513efdfdea9df54786995bb5e57
and <anders.oolong@gmail.com>
2018-11-14 16:49:03 +0800

    fixing code

--- cpp/J171_Oscar_14_Nov.cpp
+++ cpp/J171_Oscar_14_Nov.cpp
@@ -5,16 +5,26 @@ using namespace std;
 
 int main()
 {
+    //initialize variables
     char acr[100];
     int N;
     char guess[100][5000];
 
     cin >> acr;
     cin >> N;
+    
+    //get input
     cin.getline(guess[0],5000);
+
     for(int i=0;i<N;i++){
         cin.getline(guess[i],5000);
     }
+
+    //Output Marker
+    // cout << endl;
+    // cout << "Results:" << endl;
+
+    //Prcoess
     for(int i=0;i<N;i++){
         bool needCheck=true;
         int acr_i=0;
@@ -37,16 +47,16 @@ int main()
             if(guess[i][j]==32){
                 needCheck=true;
             }
-
-            j++;
         }
 
+        //Output
         if(acr_i==strlen(acr)){
             cout << guess[i] << endl;
         }else{
             cout << "No :(" << endl;
         }
-
     }
+
+
     return 0;
 }
