49ce94dcb29c9cf883a87bdf03c082a2b9948e3d
and <anders.oolong@gmail.com>
2018-11-15 15:56:24 +0800

    J171 typo, using string, 50% sb1,4 completed

--- README.md
+++ README.md
@@ -1,17 +1,47 @@
 # HKOI 16/17 samples
 
 ## Usage
-The makefile is designed to compile all `.cpp` files into `.out` files, using the same name. To execute, simply type
+
+### C
+The makefile is designed to compile all `.c` files into `.o` files, using the same name. 
+
+- To compile all files, simply type:
 ```
 make
 ```
 
-To get rid of the existing `.out` files, use *clean*
+- To compile a specific file:
+```
+make <filename> #without .c is fine
+```
+
+- To get rid of the existing `.o` files, use *clean*
 ```
 make clean
 ```
 
-## File & Git Structure
-- `.cpp` files should be placed in root
-- `.out` files are ignored in git
-- In addition, if you make a directory named `backup`, its contents will be ignored from git as well, feel free to move your files in `/backup` to avoid commiting and pushing your backup copies
+### CPP
+The makefile is designed to compile all `.cpp` files into `.out` files, using the same name.
+
+- To compile all files, simply type:
+```
+make
+```
+
+- To compile a specific file:
+```
+make <filename> #without .cpp is fine
+```
+
+- To get rid of the existing `.out` files, use *clean*
+```
+make clean
+```
+
+
+## File Structure
+_to be updated_
+
+## Git Setup
+- `.out` and `.o` files are ignored in git
+- In addition, if you make a directory named `/backup`, its contents will be ignored from git as well, feel free to move your files into `/backup` to avoid commiting and pushing your backup copies
--- cpp/J171.cpp
+++ cpp/J171.cpp
@@ -18,61 +18,63 @@ int main(int argc, char **argv)
         }
 
         //initialize variables
-        char acroym[100];           //the acroym to check for
+        string acronym;           //the acrnoym to check for
         int n;                      //the amount of inputs
-        char inputs[100][5000];     //the inputs to guess for
-        char results[100][5000];
+        string inputs[5000];     //the inputs to guess for
+        string results[5000];
 
         //get the inputs from terminal
         if(isDebugging) cout << "Acroym : ";
-        cin >> acroym;
+        getline(cin, acronym);
+
         if(isDebugging) cout << "# of inputs : ";
         cin >> n;
         cin.ignore();               //since cin >> tokenizes and leaved the newline in the stream
                                     //we need to clear it using ignore;
         for(int i=0; i<n; i++){
             if(isDebugging) cout << "Input " << i+1 << " : " ;
-            cin.getline(inputs[i],5000);
+            getline(cin, inputs[i]);
         }
 
         //Prcoess
         for(int i=0; i<n; i++){       //Loop thru inputs arr to process every input
             
-            int acr_i=0;            //counter for the acroym characters
+            results[i] = "";
+            int acr_i=0;            //counter for the acrnoym characters
             bool isInitial=true;    //if character is behind a space
                                     //Flag as an initial char for checking
             
             if(isDebugging) cout << "Debug : " << inputs[i] << " >> ";
-            for(int j=0; j<strlen(inputs[i]); j++)
+            for(int j=0; j<inputs[i].length(); j++)
             {   //Loop thru every char in input
                 
-                if(isInitial && toupper(inputs[i][j]) == acroym[acr_i])
+                if(isInitial && toupper(inputs[i][j]) == acronym[acr_i])
                 {   //Case if this char is an initial char and is part of an acronym
                     if(isDebugging) cout << "1";
-                    results[i][j] = toupper(inputs[i][j]);
+                    results[i] += toupper(inputs[i][j]) ;
                     acr_i++;
                     isInitial=false;
 
                 }else if(inputs[i][j]==' ')
                 {   //Case if this char is a space, then next char must be an initial
                     if(isDebugging) cout << " ";
-                    results[i][j] = ' ';
+                    results[i] += ' ' ;
                     isInitial=true;
 
                 }else
                 {   //Everything else should be lowercased
                     if(isDebugging) cout << "0";
-                    results[i][j] = tolower(inputs[i][j]);
+                    results[i] += tolower(inputs[i][j]);
                 }   //endif
                 
             }   //endfor
 
             
-            if(acr_i != strlen(acroym))
+            if(acr_i != acronym.length())
             {   //if the accroymn is not completely looped thru
                 //record as failure
-                if(isDebugging) cout << " >> Fail " << acr_i << "/" << strlen(acroym);
-                strcpy(results[i], "No :(");
+                if(isDebugging) cout << " >> Fail " << acr_i << "/" << acronym.length();
+                results[i] = "No :(";
             }
             
             if(isDebugging) cout << endl;
